28d340e89445f6663bcd3833ff19cd3c1e8b3ac4
MostafaMaged00 <185123397+MostafaMaged00@users.noreply.github.com>
2024-11-20 11:12:36 -0500

    more work

--- test.js
+++ test.js
@@ -1,32 +1,129 @@
-const LearnerSubmissions = [
+// The provided course information.
+const CourseInfo = {
+  id: 451,
+  name: "Introduction to JavaScript"
+};
+
+// The provided assignment group.
+const AssignmentGroup = {
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
+  assignments: [
     {
-      learner_id: 125,
-      assignment_id: 1,
-      submission: {
-        submitted_at: "2023-01-25",
-        score: 47
-      }
+      id: 1,
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50
     },
     {
-      learner_id: 125,
-      assignment_id: 2,
-      submission: {
-        submitted_at: "2023-02-12",
-        score: 150
-      }
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500
     }
-  ];
+  ]
+};
+// The provided learner submission data.
+const LearnerSubmissions = [
+  {
+    learner_id: 125,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 47
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150
+    }
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39
+    }
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140
+    }
+  }
+];
 
 
-let avg = 0
-let id = "";
-for (const stdnt of LearnerSubmissions){
+//get ids from the array 
+const Ids = [...new Set(LearnerSubmissions.map(obj => obj.learner_id))];
+console.log( Ids)
 
-    id = stdnt.learner_id
-    avg +=stdnt.submission.score
+// Creates empty objects
+const students = Array.from({ length: Ids.length }, () => ({})); 
+console.log(students)
 
-    // console.log(stdnt.learner_id,stdnt.assignment_id,stdnt.submission.score,stdnt.submission.submitted_at)
+let studId = ""
+let score1 = 0
+let score2 = 0
+let avgScore = 0
+
+for(let i = 0 ;i<=Ids.length;i++){
+
+console.log(Ids[i])
+
+
 }
-avg = avg / 200
-console.log(id)
-console.log(parseFloat(avg.toFixed(2)))
+
+LearnerSubmissions.forEach(obj => {
+
+  // studId = obj.learner_id;
+  studId = 125;
+
+  if(obj.learner_id === 125 && obj.assignment_id !== 3){
+    avgScore += obj.submission.score
+  
+  }
+
+  if(obj.learner_id === 125 && obj.assignment_id == 1){
+   score1 =  (obj.submission.score / 50).toFixed(2); 
+  }
+
+  if(obj.learner_id === 125 && obj.assignment_id == 2){
+    score2 = (obj.submission.score / 150).toFixed(2); 
+  }
+
+
+
+    
+});
+
+students[0].id = studId
+students[0].avg = avgScore
+students[0].score1 = score1
+students[0].score2 = score2
+
+
+
+console.log(students)
